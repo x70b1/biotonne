8fc2b35997d19addd1c7c604d5293f8b1bb7db86
x70b1 <x70b1@users.noreply.github.com>
2021-02-21 12:49:47 +0100

    biotonne.ino: add file

---
+++ biotonne.ino
@@ -0,0 +1,42 @@
+
+const int led_pin_red = 9;
+const int led_pin_green = 10;
+
+const int soil_pin_analog = A0;
+
+const int moisture_min = 440;
+const int moisture_max = 820;
+
+void setup() {
+    // Serial.begin(9600);
+
+    pinMode(soil_pin_analog, INPUT);
+
+    pinMode(led_pin_red, OUTPUT);
+    pinMode(led_pin_green, OUTPUT);
+    analogWrite(led_pin_red, 0);
+    analogWrite(led_pin_green, 0);
+
+    delay(100);
+    }
+
+void loop() {
+    int moisture_sensor = constrain(analogRead(soil_pin_analog), moisture_min, moisture_max);
+    int moisture_map = map(moisture_sensor, moisture_min, moisture_max, 100, 0);
+
+    // Serial.println(moisture_map);
+
+    if (moisture_map >= 40) {
+        analogWrite(led_pin_red, 0);
+        analogWrite(led_pin_green, 100);
+    } else {
+        analogWrite(led_pin_red, 255);
+        analogWrite(led_pin_green, 0);
+        delay(400);
+
+        analogWrite(led_pin_red, 0);
+        analogWrite(led_pin_green, 0);
+
+        delay(400);
+    }
+}
